86732a1841e25ab06d4fc9cbad6906de67562a14
lizaboiarchuk <yelyzaveta.boiarchuk@gmail.com>
2022-07-06 14:36:18 +0300

    onboarding finished

--- gallery-test/OnboardingViewController.swift
+++ gallery-test/OnboardingViewController.swift
@@ -6,89 +6,65 @@
 //
 
 import UIKit
+import ARKit
 
-class OnboardingViewController: UIViewController, UIScrollViewDelegate {
-
+class OnboardingViewController: UIViewController, UIScrollViewDelegate, ARSCNViewDelegate {
+    
+    private enum Configuration {
+        static let pageCount = 4
+        static let titles = ["Make sure you are indoor", "Make sure lighting is good", "It works better with textured walls", "Remove extra furniture"]
+    }
+    
+    @IBOutlet weak var transparentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet private weak var pageControl: UIPageControl!
+    @IBOutlet private weak var screenLabel: UILabel!
+    @IBOutlet private weak var sceneView: ARSCNView!
     @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var pageControl: UIPageControl!
     
-    var scrollWidth: CGFloat! = 0.0
-    var scrollHeight: CGFloat! = 0.0
     
-    var titles = ["FAST DELIVERY","EXCITING OFFERS","SECURE PAYMENT"]
-    var descs = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Lorem ipsum dolor sit amet, consectetur adipiscing elit."]
+    var screenWidth: CGFloat = 0
+    var swipedToTheEnd = false
     
-    var imgs = ["Files/test-pic","Files/test-pic","Files/test-pic"]
     
     
     override func viewDidLoad() {
-          super.viewDidLoad()
-          self.view.layoutIfNeeded()
-          //to call viewDidLayoutSubviews() and get dynamic width and height of scrollview
+        super.viewDidLoad()
+        sceneView.delegate = self
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        pageControl.numberOfPages = Configuration.pageCount
+        scrollView.contentSize = CGSize(width: transparentView.frame.width * CGFloat(Configuration.pageCount) , height: transparentView.frame.height)
+        screenWidth = transparentView.frame.width
+        startButton.isHidden = true
+        screenLabel.text = Configuration.titles[0]
+    }
 
-          self.scrollView.delegate = self
-          scrollView.isPagingEnabled = true
-          scrollView.showsHorizontalScrollIndicator = false
-          scrollView.showsVerticalScrollIndicator = false
-
-          //crete the slides and add them
-          var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-
-          for index in 0..<titles.count {
-              frame.origin.x = scrollWidth * CGFloat(index)
-              frame.size = CGSize(width: scrollWidth, height: scrollHeight)
-
-              let slide = UIView(frame: frame)
-
-              //subviews
-              let imageView = UIImageView.init(image: UIImage.init(named: imgs[index]))
-              imageView.frame = CGRect(x:0,y:0,width:300,height:300)
-              imageView.contentMode = .scaleAspectFit
-              imageView.center = CGPoint(x:scrollWidth/2,y: scrollHeight/2 - 50)
-            
-              let txt1 = UILabel.init(frame: CGRect(x:32,y:imageView.frame.maxY+30,width:scrollWidth-64,height:30))
-              txt1.textAlignment = .center
-              txt1.font = UIFont.boldSystemFont(ofSize: 20.0)
-              txt1.text = titles[index]
-
-              let txt2 = UILabel.init(frame: CGRect(x:32,y:txt1.frame.maxY+10,width:scrollWidth-64,height:50))
-              txt2.textAlignment = .center
-              txt2.numberOfLines = 3
-              txt2.font = UIFont.systemFont(ofSize: 18.0)
-              txt2.text = descs[index]
-
-              slide.addSubview(imageView)
-              slide.addSubview(txt1)
-              slide.addSubview(txt2)
-              scrollView.addSubview(slide)
-
-          }
-
-          //set width of scrollview to accomodate all the slides
-          scrollView.contentSize = CGSize(width: scrollWidth * CGFloat(titles.count), height: scrollHeight)
-
-          //disable vertical scroll/bounce
-          self.scrollView.contentSize.height = 1.0
-
-          //initial state
-          pageControl.numberOfPages = titles.count
-          pageControl.currentPage = 0
-
-      }
-
-      //indicator
-      @IBAction func pageChanged(_ sender: Any) {
-          scrollView!.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat ((pageControl?.currentPage)!), y: 0, width: scrollWidth, height: scrollHeight), animated: true)
-      }
-
-      func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-          setIndiactorForCurrentPage()
-      }
-
-      func setIndiactorForCurrentPage()  {
-          let page = (scrollView?.contentOffset.x)!/scrollWidth
-          pageControl?.currentPage = Int(page)
-      }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        var page : Int = Int(round(scrollView.contentOffset.x / screenWidth))
+        if page < 0 { page = 0 }
+        if page > Configuration.pageCount - 1  { page = Configuration.pageCount }
+        
+        pageControl.currentPage = page
+        configurePage(number: page)
+        
+    }
+    
+    private func configurePage(number page: Int) {
+        if page >= Configuration.pageCount - 1 { swipedToTheEnd = true }
+        if swipedToTheEnd {
+            startButton.isHidden = false
+        }
+        
+        screenLabel.text = Configuration.titles[page]
+    }
   }
+    
