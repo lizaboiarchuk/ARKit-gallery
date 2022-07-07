abb20cf96c8407e8d64f7ea8dddf7cdcbabe7284
lizaboiarchuk <yelyzaveta.boiarchuk@gmail.com>
2022-07-07 09:57:06 +0300

    fix orientation

---
+++ gallery-test/UIImage.swift
@@ -0,0 +1,22 @@
+//
+//  UIImage.swift
+//  gallery-test
+//
+//  Created by Yelyzaveta Boiarchuk on 07.07.2022.
+//
+
+import Foundation
+
+extension UIImage {
+    
+    func fixOrientation() -> UIImage {
+        if self.imageOrientation == UIImage.Orientation.up { return self }
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        let normalizedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return normalizedImage;
+    }
+
+}
+
--- gallery-test/ViewController.swift
+++ gallery-test/ViewController.swift
@@ -266,17 +266,3 @@ struct ReversedGeoLocation {
     }
 }
 
-
-
-extension UIImage {
-    
-    func fixOrientation() -> UIImage {
-        if self.imageOrientation == UIImage.Orientation.up { return self }
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        let normalizedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return normalizedImage;
-    }
-
-}
